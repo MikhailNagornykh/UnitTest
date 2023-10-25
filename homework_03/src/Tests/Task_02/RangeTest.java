d5a980dc296c5f8f84629ebcfe72afd35d4bd822
Mikhail Nagornykh <stumpik@gmail.com>
2023-10-25 18:26:43 +0300

    HW_2

---
+++ homework_03/src/Tests/Task_02/RangeTest.java
@@ -0,0 +1,26 @@
+package Task_02;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RangeTest {
+    private Range range;
+
+    @BeforeEach
+    public void createClassRange(){
+        range = new Range();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {26, 50 , 99})
+    void numberInIntervalIsTrue(int number) {
+        assertTrue(range.numberInInterval(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {25, 100, -1, 1000})
+    void numberInIntervalIsFalse(int number) {
+        assertFalse(range.numberInInterval(number));
+    }
+}
